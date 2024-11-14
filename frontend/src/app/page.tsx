46a22e65fff731c06e663011736aeee207374e43
JinnySarita <6432181821@student.chula.ac.th>
2024-11-14 20:38:43 +0700

    [add] NextAuthProvider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-// import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/authOptions";
 import { Link } from "@mui/material";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex font-sans">Hello</div>
