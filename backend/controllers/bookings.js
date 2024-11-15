122abc87060f5e7da9df0e4c193c9dbd474cff9f
Papon <papon.k@houseofdev.tech>
2024-11-15 21:24:42 +0700

    [ADD] Populate user to reservations

--- backend/controllers/bookings.js
+++ backend/controllers/bookings.js
@@ -9,8 +9,7 @@ const { sendMail } = require("./mails");
 exports.getBookings = async (req, res, next) => {
   let query;
   let coworkingspaceId = req.query["coworkingspaceId"];
-  if (req.user.role !== "admin") 
-  {
+  if (req.user.role !== "admin") {
     if (coworkingspaceId) {
       query = Booking.find({
         user: req.user.id,
@@ -27,19 +26,22 @@ exports.getBookings = async (req, res, next) => {
         select: "name address tel",
       });
     }
-  } 
-  else 
-  {
+  } else {
     if (coworkingspaceId) {
       query = Booking.find({ coworkingspace: coworkingspaceId }).populate({
         path: "coworkingspace",
         select: "name address tel",
       });
     } else {
-      query = Booking.find().populate({
-        path: "coworkingspace",
-        select: "name address tel",
-      });
+      query = Booking.find()
+        .populate({
+          path: "coworkingspace",
+          select: "name address tel",
+        })
+        .populate({
+          path: "user",
+          select: "name",
+        });
     }
   }
   try {
@@ -62,13 +64,12 @@ exports.getBookings = async (req, res, next) => {
 //@access   Private
 exports.getBooking = async (req, res, next) => {
   let booking;
-  try 
-  {
+  try {
     booking = await Booking.findById(req.params.id).populate({
       path: "coworkingspace",
       select: "name address tel",
     });
-    
+
     if (!booking) {
       return res.status(404).json({
         success: false,
@@ -84,8 +85,7 @@ exports.getBooking = async (req, res, next) => {
     }
 
     res.status(200).json({ success: true, data: booking });
-  } 
-  catch (error) {
+  } catch (error) {
     console.log(error);
     return res
       .status(500)
@@ -97,18 +97,22 @@ exports.getBooking = async (req, res, next) => {
 //@route    POST /api/v1/coworkingspaces/:coworkingspaceId/bookings
 //@access   Private
 exports.addBooking = async (req, res, next) => {
+  if (!req.body.numOfRooms) {
+    req.body.numOfRooms = 1;
+  }
 
-  if(!req.body.numOfRooms) { req.body.numOfRooms = 1};
-
-  if((req.body.numOfRooms<1) || (req.body.numOfRooms>3)) {
-    return res.status(400).json({ 
-      success: false, 
-      message: `User can reserve 1-3 rooms` });
+  if (req.body.numOfRooms < 1 || req.body.numOfRooms > 3) {
+    return res.status(400).json({
+      success: false,
+      message: `User can reserve 1-3 rooms`,
+    });
   }
 
   try {
     req.body.coworkingspace = req.params.coworkingspaceId;
-    const coworkingspace = await Coworkingspace.findById(req.params.coworkingspaceId);
+    const coworkingspace = await Coworkingspace.findById(
+      req.params.coworkingspaceId
+    );
     if (!coworkingspace) {
       return res.status(404).json({
         success: false,
@@ -118,17 +122,17 @@ exports.addBooking = async (req, res, next) => {
     req.body.user = req.user.id;
 
     const booking = await Booking.create(req.body);
-      res.status(200).json({
-        success: true,
-        data: booking,
-      });
+    res.status(200).json({
+      success: true,
+      data: booking,
+    });
 
-      User.findById(req.user.id, function (err, user) {
-        if (err) {
-          console.log(err);
-        } else {
-          sendMail(user, booking);
-        }
+    User.findById(req.user.id, function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        sendMail(user, booking);
+      }
     });
   } catch (error) {
     console.log(error);
@@ -143,11 +147,14 @@ exports.addBooking = async (req, res, next) => {
 //@route    PUT /api/v1/bookings/:id
 //@access   Private
 exports.updateBooking = async (req, res, next) => {
-
-  if((req.body.numOfRooms) && (((req.body.numOfRooms<1))||((req.body.numOfRooms>3)))) {
-    return res.status(400).json({ 
-      success: false, 
-      message: `User can reserve 1-3 rooms` });
+  if (
+    req.body.numOfRooms &&
+    (req.body.numOfRooms < 1 || req.body.numOfRooms > 3)
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: `User can reserve 1-3 rooms`,
+    });
   }
 
   try {
@@ -165,7 +172,7 @@ exports.updateBooking = async (req, res, next) => {
       });
     }
 
-    if(!req.body.numOfRooms) {
+    if (!req.body.numOfRooms) {
       req.body.numOfRooms = booking.numOfRooms;
     }
 
