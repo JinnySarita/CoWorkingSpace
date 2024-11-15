a8810e8ad5b5d32dede0b04cf9a8160b48554b0c
JinnySarita <6432181821@student.chula.ac.th>
2024-11-15 19:05:25 +0700

    [add]  LanguageSwitcher to auth

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-full items-center justify-center">
       {children}
-      <LanguageSwitcher className="absolute right-8 bottom-8" />
+      <LanguageSwitcher className="absolute right-8 bottom-8 text-[#1976D2]" />
     </div>
   );
 }
