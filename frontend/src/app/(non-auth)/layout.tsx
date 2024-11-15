8b97077a5a8de4b96ab9fdda9434b42740a97909
Papon <papon.k@houseofdev.tech>
2024-11-15 22:41:07 +0700

    [FIX] Werid Layout

--- frontend/src/app/(non-auth)/layout.tsx
+++ frontend/src/app/(non-auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-full min-h-screen flex flex-col">
       <NavBar />
-      <div className="flex-grow flex flex-col px-4 md:px-8 lg:px-16 2xl:px-32 py-2 md:py-4 xl:py-8 2xl:py-16 w-full">
+      <div className="flex-grow flex flex-col px-4 md:px-8 lg:px-16 2xl:px-32 py-4 md:py-8 lg:py-16 w-full">
         {children}
       </div>
       <Footer />
