a0aa1de6445cadc3da1b9d08509f4ea8cd14ab49
Papon <papon.k@houseofdev.tech>
2024-11-16 02:50:13 +0700

    [ADD] Update data

---
+++ frontend/src/app/(non-auth)/reservations/[slug]/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import ReservationForm from "@/components/create_edit_reservation/ReservationForm";
+import getReservation from "@/libs/getReservation";
+import putReservation from "@/libs/putReservation";
+import { Alert, Snackbar, Typography } from "@mui/material";
+import { Dayjs } from "dayjs";
+import { useSession } from "next-auth/react";
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+// Define the type of reservation
+interface Reservation {
+  coworkingspace: any;
+  numOfRooms: number;
+  bookingDate: string;
+}
+
+export default function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const t = useTranslations("reservations.form");
+  const session = useSession();
+  const router = useRouter();
+
+  const [reservation, setReservation] = useState<Reservation | null>(null);
+
+  async function fetchReservation() {
+    const id = (await params).slug;
+    try {
+      const response = await getReservation(session.data?.user.token!, id);
+      if (!response.success) {
+        router.push("/reservations");
+      }
+      setReservation(response.data);
+    } catch (error) {
+      router.push("/reservations");
+    }
+  }
+
+  useEffect(() => {
+    fetchReservation();
+  }, []);
+
+  async function handleSubmit({
+    coWorkingSpaceID,
+    numberOfRooms,
+    reservationDate,
+  }: {
+    coWorkingSpaceID: string;
+    numberOfRooms: number;
+    reservationDate: Dayjs | null;
+  }) {
+    try {
+      const id = (await params).slug;
+      const response = await putReservation(
+        session.data?.user.token!,
+        id,
+        coWorkingSpaceID,
+        reservationDate!.format("YYYY-MM-DD"),
+        numberOfRooms
+      );
+      if (!response.success) {
+        setError(t("error-updating-reservation"));
+        return;
+      }
+      router.push(`/reservations`);
+    } catch (error) {
+      setError(t("error-updating-reservation"));
+    }
+  }
+
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const setError = (message: string) => {
+    setSnackbarMessage(message);
+    setSnackbarOpen(true);
+  };
+
+  return (
+    reservation && (
+      <div className="flex flex-col gap-8">
+        <Typography variant="h4">{t("edit-title")}</Typography>
+        <Typography variant="h5">{t("edit-description")}</Typography>
+        <ReservationForm
+          onSubmit={handleSubmit}
+          coWorkingSpaceID={reservation.coworkingspace.id}
+          numberOfRooms={reservation.numOfRooms}
+          reservationDate={reservation.bookingDate}
+        />
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={() => setSnackbarOpen(false)}
+        >
+          <Alert
+            onClose={() => setSnackbarOpen(false)}
+            severity="error"
+            sx={{ width: "100%" }}
+          >
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
+      </div>
+    )
+  );
+}
